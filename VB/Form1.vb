8f76d681af65a5b144571f44f9c17521d4ab6bf0
DevExpressExampleBot <fakeEmail>
2023-12-10 17:02:45 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,55 +1,47 @@
-﻿Imports System.Windows.Forms
+Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
 
 Namespace ChartInteractivePoints
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Shared lastY As Integer = -1
-		Private Shared isPressed As Boolean = False
-		Private Shared selectedPoint As SeriesPoint = Nothing
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub chartControl1_ObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs) Handles chartControl1.ObjectHotTracked
-			If e.HitInfo.SeriesPoint IsNot Nothing Then
-				selectedPoint = e.HitInfo.SeriesPoint
-			End If
+        Private Shared lastY As Integer = -1
 
-			If selectedPoint IsNot Nothing AndAlso isPressed Then
-				Dim point As DiagramCoordinates = CType((TryCast(sender, ChartControl)).Diagram, XYDiagram).PointToDiagram(e.HitInfo.HitPoint)
+        Private Shared isPressed As Boolean = False
 
-				If lastY <> -1 Then
-					Dim range As AxisRange = CType((TryCast(sender, ChartControl)).Diagram, XYDiagram).AxisY.Range
-					Dim delta As Double = (CDbl(range.MaxValue) - CDbl(range.MinValue)) / 8
+        Private Shared selectedPoint As SeriesPoint = Nothing
 
-					If selectedPoint.Values(0) >= CDbl(range.MaxValue) - delta Then
-						range.MaxValue = selectedPoint.Values(0) + delta
-					End If
+        Private Sub chartControl1_ObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs)
+            If e.HitInfo.SeriesPoint IsNot Nothing Then selectedPoint = e.HitInfo.SeriesPoint
+            If selectedPoint IsNot Nothing AndAlso isPressed Then
+                Dim point As DiagramCoordinates = CType(TryCast(sender, ChartControl).Diagram, XYDiagram).PointToDiagram(e.HitInfo.HitPoint)
+                If lastY <> -1 Then
+                    Dim range As AxisRange = CType(TryCast(sender, ChartControl).Diagram, XYDiagram).AxisY.Range
+                    Dim delta As Double =(CDbl(range.MaxValue) - CDbl(range.MinValue)) / 8
+                    If selectedPoint.Values(0) >= CDbl(range.MaxValue) - delta Then range.MaxValue = selectedPoint.Values(0) + delta
+                    selectedPoint.Values(0) = point.NumericalValue
+                    If Not Equals(point.QualitativeArgument, "") Then selectedPoint.Argument = point.QualitativeArgument
+                End If
 
-					selectedPoint.Values(0) = point.NumericalValue
+                CType(sender, ChartControl).RefreshData()
+                lastY = e.HitInfo.HitPoint.Y
+                Return
+            End If
 
-					If point.QualitativeArgument <> "" Then
-						selectedPoint.Argument = point.QualitativeArgument
-					End If
-				End If
+            lastY = -1
+        End Sub
 
-				DirectCast(sender, ChartControl).RefreshData()
-				lastY = e.HitInfo.HitPoint.Y
-				Return
-			End If
+        Private Sub chartControl1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+            isPressed = True
+        End Sub
 
-			lastY = -1
-		End Sub
-
-		Private Sub chartControl1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles chartControl1.MouseDown
-			isPressed = True
-		End Sub
-
-		Private Sub chartControl1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles chartControl1.MouseUp
-			isPressed = False
-		End Sub
-	End Class
+        Private Sub chartControl1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs)
+            isPressed = False
+        End Sub
+    End Class
 End Namespace
