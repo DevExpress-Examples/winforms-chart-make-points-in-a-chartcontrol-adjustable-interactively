6fe205db17c9eba1f5488cbeb5859f2f05c8d025
DevExpressExampleBot <fakeEmail>
2025-03-14 13:06:21 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace ChartInteractivePoints
-    Partial Public Class Form1
+Namespace ChartInteractivePoints
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,37 +12,37 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
-            Dim series1 As New DevExpress.XtraCharts.Series()
-            Dim pointSeriesLabel1 As New DevExpress.XtraCharts.PointSeriesLabel()
-            Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("a", New Object() { (DirectCast(1R, Object))})
-            Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("b", New Object() { (DirectCast(2R, Object))})
-            Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("c", New Object() { (DirectCast(2R, Object))})
-            Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("d", New Object() { (DirectCast(1R, Object))})
-            Dim lineSeriesView1 As New DevExpress.XtraCharts.LineSeriesView()
-            Dim pointSeriesLabel2 As New DevExpress.XtraCharts.PointSeriesLabel()
-            Dim lineSeriesView2 As New DevExpress.XtraCharts.LineSeriesView()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel1 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim seriesPoint1 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("a", New Object() {(CObj((1R)))})
+            Dim seriesPoint2 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("b", New Object() {(CObj((2R)))})
+            Dim seriesPoint3 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("c", New Object() {(CObj((2R)))})
+            Dim seriesPoint4 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("d", New Object() {(CObj((1R)))})
+            Dim lineSeriesView1 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
+            Dim pointSeriesLabel2 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim lineSeriesView2 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(lineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(lineSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((lineSeriesView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((lineSeriesView2), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' chartControl1
@@ -48,32 +50,32 @@
             xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
             xyDiagram1.AxisX.WholeRange.AutoSideMargins = True
             xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-            xyDiagram1.AxisY.VisualRange.Auto = False
-            xyDiagram1.AxisY.VisualRange.AutoSideMargins = True
-            xyDiagram1.AxisY.VisualRange.MaxValueSerializable = "2.3"
-            xyDiagram1.AxisY.VisualRange.MinValueSerializable = "0"
             xyDiagram1.AxisY.WholeRange.Auto = False
-            xyDiagram1.AxisY.WholeRange.AutoSideMargins = True
-            xyDiagram1.AxisY.WholeRange.MaxValueSerializable = "2.3"
+            xyDiagram1.AxisY.WholeRange.AutoSideMargins = False
+            xyDiagram1.AxisY.WholeRange.SideMarginsValue = 0
+            xyDiagram1.AxisY.WholeRange.MaxValueSerializable = "2.5"
             xyDiagram1.AxisY.WholeRange.MinValueSerializable = "0"
             Me.chartControl1.Diagram = xyDiagram1
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.chartControl1.Location = New System.Drawing.Point(0, 0)
             Me.chartControl1.Name = "chartControl1"
             Me.chartControl1.RuntimeHitTesting = True
-            pointSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            pointSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
             pointSeriesLabel1.TextPattern = "{V:N2}"
             series1.Label = pointSeriesLabel1
             series1.Name = "Series 1"
-            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
-            lineSeriesView1.MarkerVisibility = DevExpress.Utils.DefaultBoolean.True
+            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
+            lineSeriesView1.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
             series1.View = lineSeriesView1
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
-            pointSeriesLabel2.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series1}
+            pointSeriesLabel2.LineVisibility = DevExpress.Utils.DefaultBoolean.[True]
             Me.chartControl1.SeriesTemplate.Label = pointSeriesLabel2
             Me.chartControl1.SeriesTemplate.View = lineSeriesView2
             Me.chartControl1.Size = New System.Drawing.Size(466, 344)
             Me.chartControl1.TabIndex = 0
+            AddHandler Me.chartControl1.ObjectHotTracked, New DevExpress.XtraCharts.HotTrackEventHandler(AddressOf Me.chartControl1_ObjectHotTracked)
+            AddHandler Me.chartControl1.MouseDown, New System.Windows.Forms.MouseEventHandler(AddressOf Me.chartControl1_MouseDown)
+            AddHandler Me.chartControl1.MouseUp, New System.Windows.Forms.MouseEventHandler(AddressOf Me.chartControl1_MouseUp)
             ' 
             ' Form1
             ' 
@@ -83,20 +85,17 @@
             Me.Controls.Add(Me.chartControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(lineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(lineSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((lineSeriesView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((pointSeriesLabel2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((lineSeriesView2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
-        Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
+'#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
     End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,8 +1,9 @@
-﻿Imports System.Windows.Forms
+Imports System.Windows.Forms
 Imports DevExpress.XtraCharts
 
 Namespace ChartInteractivePoints
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
@@ -10,33 +11,25 @@ Namespace ChartInteractivePoints
         End Sub
 
         Private Shared lastY As Integer = -1
+
         Private Shared isPressed As Boolean = False
+
         Private Shared selectedPoint As SeriesPoint = Nothing
 
-        Private Sub chartControl1_ObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs) Handles chartControl1.ObjectHotTracked
-            If e.HitInfo.SeriesPoint IsNot Nothing Then
-                selectedPoint = e.HitInfo.SeriesPoint
-            End If
-
+        Private Sub chartControl1_ObjectHotTracked(ByVal sender As Object, ByVal e As HotTrackEventArgs)
+            If e.HitInfo.SeriesPoint IsNot Nothing Then selectedPoint = e.HitInfo.SeriesPoint
             If selectedPoint IsNot Nothing AndAlso isPressed Then
-                Dim point As DiagramCoordinates = CType((TryCast(sender, ChartControl)).Diagram, XYDiagram).PointToDiagram(e.HitInfo.HitPoint)
-
+                Dim point As DiagramCoordinates = CType(TryCast(sender, ChartControl).Diagram, XYDiagram).PointToDiagram(e.HitInfo.HitPoint)
                 If lastY <> -1 Then
-                    Dim range As VisualRange = CType((TryCast(sender, ChartControl)).Diagram, XYDiagram).AxisY.VisualRange
-                    Dim delta As Double = (CDbl(range.MaxValue) - CDbl(range.MinValue)) / 8
-
-                    If selectedPoint.Values(0) >= CDbl(range.MaxValue) - delta Then
-                        range.MaxValue = selectedPoint.Values(0) + delta
-                    End If
-
+                    ' Update AxisY.WholeRange if the point is too close to diagram bounds
+                    Dim range As WholeRange = CType(TryCast(sender, ChartControl).Diagram, XYDiagram).AxisY.WholeRange
+                    Dim delta As Double =(CDbl(range.MaxValue) - CDbl(range.MinValue)) / 8
+                    If selectedPoint.Values(0) >= CDbl(range.MaxValue) - delta Then range.MaxValue = selectedPoint.Values(0) + delta
                     selectedPoint.Values(0) = point.NumericalValue
-
-                    If point.QualitativeArgument <> "" Then
-                        selectedPoint.Argument = point.QualitativeArgument
-                    End If
+                    If Not Equals(point.QualitativeArgument, "") Then selectedPoint.Argument = point.QualitativeArgument
                 End If
 
-                DirectCast(sender, ChartControl).RefreshData()
+                CType(sender, ChartControl).RefreshData()
                 lastY = e.HitInfo.HitPoint.Y
                 Return
             End If
@@ -44,11 +37,11 @@ Namespace ChartInteractivePoints
             lastY = -1
         End Sub
 
-        Private Sub chartControl1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles chartControl1.MouseDown
+        Private Sub chartControl1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
             isPressed = True
         End Sub
 
-        Private Sub chartControl1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles chartControl1.MouseUp
+        Private Sub chartControl1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs)
             isPressed = False
         End Sub
     End Class
